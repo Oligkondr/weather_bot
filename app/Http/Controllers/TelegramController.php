ab38b504e3836a76a1a815dc83323b9c36437944
olegk <oligkondr@mail.ru>
2024-07-19 21:25:17 +0300

    Add start handler

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -2,25 +2,54 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Client;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
 class TelegramController extends Controller
 {
+    private array $message;
+
     public function webhook()
     {
-        $message = request('message');
+        $this->message = request('message');
 
-        switch ($message['text']) {
+        switch ($this->message['text']) {
             case '/start':
-
+                $this->start();
                 break;
 
             default:
                 Telegram::sendMessage([
-                    'chat_id' => $message['chat']['id'],
-                    'text' => "{$message['from']['first_name']}, твоя команда - говно."
+                    'chat_id' => $this->message['chat']['id'],
+                    'text' => "{$this->message['from']['first_name']}, вы отправили неизвестную команду.",
                 ]);
         }
+    }
+
+    private function start()
+    {
+        $client = Client::query()
+            ->where('ext_id', $this->message['from']['id'])
+            ->first();
+
+        if ($client) {
+            $text = 'вы уже с нами!';
+        } else {
+            $client = new Client();
+            $client->ext_id = $this->message['from']['id'];
+
+            $text = 'мы рады, что вы теперь с нами!';
+        }
+
+        Telegram::sendMessage([
+            'chat_id' => $this->message['chat']['id'],
+            'text' => "{$this->message['from']['first_name']}, {$text}",
+        ]);
+
+        $client->first_name = $this->message['from']['first_name'];
+        $client->last_name = $this->message['from']['last_name'];
+        $client->username = $this->message['from']['username'];
+        $client->language_code = $this->message['from']['language_code'];
+        $client->save();
     }
 }
--- app/Models/Client.php
+++ app/Models/Client.php
@@ -5,6 +5,32 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $ext_id
+ * @property string $first_name
+ * @property string $last_name
+ * @property string $username
+ * @property string $language_code
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property int $is_disable
+ * @method static \Illuminate\Database\Eloquent\Builder|Client newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Client newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Client query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Client whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Client whereExtId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Client whereFirstName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Client whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Client whereIsDisable($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Client whereLanguageCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Client whereLastName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Client whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Client whereUsername($value)
+ * @mixin \Eloquent
+ */
 class Client extends Model
 {
     use HasFactory;
