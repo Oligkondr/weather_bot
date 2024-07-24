8f8a3d35f994f4722899336b84cc70d800522332
olegk <oligkondr@mail.ru>
2024-07-24 14:31:11 +0300

    add photo to message

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -329,7 +329,8 @@ class TelegramController extends Controller
     {
         Telegram::sendPhoto([
             'chat_id' => $this->chatId,
-            'photo' => InputFile::create('https://telegram-bot-sdk.com/img/hero-banner.png'),
+//            'photo' => InputFile::create('https://telegram-bot-sdk.com/img/hero-banner.png'),
+            'photo' => InputFile::create('https://openweathermap.org/img/wn/10d@4x.png'),
             'caption' => 'Test',
         ]);
     }
