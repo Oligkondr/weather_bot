6a0e21d036bace830f6209eef57c63c2698ce8ff
olegk <oligkondr@mail.ru>
2024-08-01 15:08:53 +0300

    New migration

---
+++ resources/js/Components/Header.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="bg-[#1f2937] p-4 text-white">
+    123
+</div>
+</template>
+
+<style scoped>
+p {
+    background-color: #9ab5d5;
+}
+</style>
---
+++ resources/js/Components/Main.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="bg-[#9ab5d5]">
+        123
+    </div>
+    <div class="bg-[#1f2937] p-4 text-white">
+        123
+    </div>
+</template>
+
+<style scoped>
+
+</style>
