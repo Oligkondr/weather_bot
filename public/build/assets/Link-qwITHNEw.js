88dc9df8f3767333e0eeade045d0deb066e0cfa5
olegk <oligkondr@mail.ru>
2024-08-02 13:51:20 +0300

    Build

---
+++ public/build/assets/Link-qwITHNEw.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{o,f as r}from"./app-BfSn2yOt.js";const c={};function n(t,s){return o(),r("div",null," 123 ")}const f=e(c,[["render",n]]);export{f as default};
